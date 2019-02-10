caa23635fb8b922e84c1ab8bf9c205c60d41bda1
tungfaifong <iam@tungfaifong.com>
2019-02-10 20:52:17 +0800

    删除调试代码

--- src/util.lua
+++ src/util.lua
@@ -68,35 +68,3 @@ function table2Json(lua_table)
     local json = require("dkjson")
     return json.encode( lua_table , {indent = true} )
 end
-
-printt = function(lua_table, indent)
-    if type(lua_table) ~= "table" then
-        print(lua_table)
-        return
-    end
-    indent = indent or 0
-    for k, v in pairs(lua_table) do
-        if type(k) == "string" then
-            k = string.format("%q", k)
-        end
-        local szSuffix = ""
-        if type(v) == "table" then
-            szSuffix = "{"
-        end
-        local szPrefix = string.rep("    ", indent)
-        formatting = szPrefix .. "[" .. k .. "]" .. " = " .. szSuffix
-        if type(v) == "table" then
-            print(formatting)
-            printt(v, indent + 1)
-            print(szPrefix .. "},")
-        else
-            local szValue = ""
-            if type(v) == "string" then
-                szValue = string.format("%q", v)
-            else
-                szValue = tostring(v)
-            end
-            print(formatting .. szValue .. ",")
-        end
-    end
-end
