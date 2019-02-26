5050258f2a34fe1d79032a2dae37cbed0ff6aea7
tungfaifong <iam@tungfaifong.com>
2019-02-26 16:39:55 +0800

    更新到lua5.3、新增文件名备注、支持浮点数

--- lua/config.lua
+++ lua/config.lua
@@ -1,8 +1,10 @@
-require("luacom")
-require("lfs")
+luacom = require("luacom")
+luaiconv = require("luaiconv")
+winfile = require("winfile")
 
 --初始化下lua代码路径
-ROOT_PATH = lfs.currentdir()
+ROOT_PATH = winfile.currentdir()
+
 SRC_PATH = ROOT_PATH .. "\\src"
 EXCEL_PATH = ROOT_PATH .. "\\excel"
 OUT_PATH = ROOT_PATH .. "\\out"
