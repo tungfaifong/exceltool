33465bbcc0e971a914925ad6ef3a6b50c3663df0
tungfaifong <iam@tungfaifong.com>
2019-10-26 17:16:51 +0800

    重构生成逻辑

--- src/generate.lua
+++ src/generate.lua
@@ -6,7 +6,7 @@ DATA_START_INDEX = 5
 
 FLOAT_FIX = 0.0000000000005
 
-function get_value(data_type, value)
+local function getValue(data_type, value)
 	if string.find(data_type, "bool") then
 		return tonumber(value) > 0
 	elseif string.find(data_type, "int") then
@@ -20,10 +20,46 @@ function get_value(data_type, value)
 	end
 end
 
+local function addSortMark(prefix, key, sort_mark)
+	if not prefix or not key then
+		return sort_mark
+	end
+
+	prefix["#sort#"] = prefix["#sort#"] or {}
+
+	if prefix["#sort#"][key] then
+		return sort_mark
+	end
+
+	prefix["#sort#"][key] = sort_mark
+	return sort_mark + 1
+end
+
+local function getPrefix(config, id_list, id_index)
+	if id_index + 1 > #id_list then
+		return config, id_list[id_index]
+	end
+
+	local i = id_list[id_index]
+	config[i] = config[i] or {}
+	
+	return getPrefix(config[i], id_list, id_index + 1)
+end
+
+local function getArray(config, index_list, index)
+	if index > #index_list then
+		return config
+	end
+
+	local i = tonumber(index_list[index])
+	config[i] = config[i] or {}
+
+	return getArray(config[i], index_list, index + 1)
+end
+
 function generate(path, type)
 	local book = excel.Workbooks:Open(path, nil, true)
 	local config = {}
-	config["#sort#"] = {}
 	local sort_mark = 1
 
 	for i = 1, book.Sheets.Count do
@@ -38,7 +74,6 @@ function generate(path, type)
 			table.remove(sheet_data, 1)
 		end
 
-		local obj_array_flag = false
 		local obj_array_prefix = nil
 		local obj_array_offset = 0
 		
@@ -47,146 +82,76 @@ function generate(path, type)
 			local out_type = v[OUT_TYPE_INDEX]
 			if id and string.sub(id, 1, 1) ~= '#' and (out_type == nil or out_type == type) then
 				local id_list = stringSplit(id, ':')
-				local key = id_list[1]
-
-				if key and not config["#sort#"][key] then
-					config["#sort#"][key] = sort_mark
-					sort_mark = sort_mark + 1
-				end
-
-				local type_list = stringSplit(v[TYPE_INDEX], ':')
-				local data_type = type_list[1]
-				local key_child = id_list[2]
-
-				if not key_child then
-					obj_array_flag = false
+				if #id_list <= 1 then
 					obj_array_prefix = nil
 					obj_array_offset = 0
 				end
 
+				local prefix, key = getPrefix(config, id_list, 1)
+				prefix = obj_array_prefix or prefix
+
+				local type_list = stringSplit(v[TYPE_INDEX], ':')
+				local data_type = type_list[1]
+
 				if string.find(data_type, "%[%]") then
-					config[key] = config[key] or {}
+					local function getTargetArray(prefix, key, type_list)
+						prefix[key] = prefix[key] or {}
+						sort_mark = addSortMark(prefix, key, sort_mark)
 
-					local target_config = config[key]
+						local target_array = prefix[key]
 
-					local is_multi_array = false
+						if type_list[2] then
+							local index_list = string.sub(type_list[2], 2, #type_list[2] - 1)
+							index_list = string.gsub(index_list, '%]%[', ':')
+							index_list = stringSplit(index_list, ':')
 
-					if type_list[2] then
-						local index_list = string.sub(type_list[2], 2, #type_list[2] - 1)
-						index_list = string.gsub(index_list, '%]%[', ':')
-						index_list = stringSplit(index_list, ':')
-
-						local code = "function create_array(config)\n"
-						local config_code =  "config"
-						for _, index in pairs(index_list) do
-							config_code = config_code .. "[" .. index .. "]"
-							code = code .. config_code .. " = " .. config_code .. " or {}\n"
+							target_array = getArray(prefix[key], index_list, 1)
 						end
-						code = code .. "return " .. config_code .. "\n"
-						code = code .. "end"
 
-						loadstring(code)()
-
-						is_multi_array = true
+						return target_array
 					end
 
-					if string.find(data_type, "obj") then
-						obj_array_flag = true
-						obj_array_prefix = is_multi_array and create_array(config[key]) or config[key]
-						obj_array_offset = #obj_array_prefix
+					if obj_array_prefix then
+						for i = DATA_START_INDEX, table.maxn(v) do
+							local index = i - DATA_START_INDEX + 1 + obj_array_offset
+							prefix[index] = prefix[index] or {}
+							local target_array = getTargetArray(prefix[index], key, type_list)
+
+							if v[i] then
+								table.insert(target_array, getValue(data_type, v[i]))
+							end
+						end
 					else
-						if obj_array_flag then
-							target_config = {}
-							for i = DATA_START_INDEX, table.maxn(v) do
-								local index = i - DATA_START_INDEX + 1 + obj_array_offset
-								obj_array_prefix[index] = obj_array_prefix[index] or {}
-								obj_array_prefix[index]["#sort#"] = obj_array_prefix[index]["#sort#"] or {}
+						local target_array = getTargetArray(prefix, key, type_list)
 
-								obj_array_prefix[index][key_child] = obj_array_prefix[index][key_child] or {}
-								target_config[index] = obj_array_prefix[index][key_child]
-
-								if not obj_array_prefix[index]["#sort#"][key_child] then
-									obj_array_prefix[index]["#sort#"][key_child] = sort_mark
-									sort_mark = sort_mark + 1
-								end
-							end
-						else
-							if key_child then
-								config[key]["#sort#"] = config[key]["#sort#"] or {}
-								config[key][key_child] = config[key][key_child] or {}
-								target_config = config[key][key_child]
-
-								if not config[key]["#sort#"][key_child] then
-									config[key]["#sort#"][key_child] = sort_mark
-									sort_mark = sort_mark + 1
-								end
-							end
-						end
-
-						if is_multi_array then
-							if obj_array_flag then
-								for i = DATA_START_INDEX, table.maxn(v) do
-									local index = i - DATA_START_INDEX + 1 + obj_array_offset
-									target_config[index] = create_array(target_config[index])
-								end
-							else
-								target_config = create_array(target_config)
-							end
-						end
-
-						if key_child then
-							if obj_array_flag then
-								for i = DATA_START_INDEX, table.maxn(v) do
-									if v[i] then
-										local index = i - DATA_START_INDEX + 1 + obj_array_offset
-										table.insert(target_config[index], get_value(data_type, v[i]))
-									end
-								end
-							else
-								if v[DATA_START_INDEX] then
-									table.insert(target_config, get_value(data_type, v[DATA_START_INDEX]))
-								end
-							end
+						if string.find(data_type, "obj") then
+							obj_array_prefix = target_array
+							obj_array_offset = #obj_array_prefix
 						else
 							for i = DATA_START_INDEX, table.maxn(v) do
 								if v[i] then
-									table.insert(target_config, get_value(data_type, v[i]))
+									table.insert(target_array, getValue(data_type, v[i]))
 								end
 							end
 						end
-					end					
+					end
 				else
-					if obj_array_flag then
+					local function addValue(prefix, key, data_type, value)
+						prefix[key] = getValue(data_type, value)
+						sort_mark = addSortMark(prefix, key, sort_mark)
+					end
+
+					if obj_array_prefix then
 						for i = DATA_START_INDEX, table.maxn(v) do
-							if v[i] or string.find(data_type, "obj") then
-								local value = get_value(data_type, v[i])
-
+							if v[i] then
 								local index = i - DATA_START_INDEX + 1 + obj_array_offset
-								obj_array_prefix[index] = obj_array_prefix[index] or {}
-								obj_array_prefix[index]["#sort#"] = obj_array_prefix[index]["#sort#"] or {}
-								obj_array_prefix[index][key_child] = value
-
-								if not obj_array_prefix[index]["#sort#"][key_child] then
-									obj_array_prefix[index]["#sort#"][key_child] = sort_mark
-									sort_mark = sort_mark + 1
-								end
+								prefix[index] = prefix[index] or {}
+								addValue(prefix[index], key, data_type, v[i])
 							end
 						end
 					else
-						if v[DATA_START_INDEX] or string.find(data_type, "obj") then
-							local value = get_value(data_type, v[DATA_START_INDEX])
-
-							if key_child then
-								config[key]["#sort#"] = config[key]["#sort#"] or {}
-								config[key][key_child] = value
-
-								if not config[key]["#sort#"][key_child] then
-									config[key]["#sort#"][key_child] = sort_mark
-									sort_mark = sort_mark + 1
-								end
-							else
-								config[key] = value
-							end
+						if v[DATA_START_INDEX] then
+							addValue(prefix, key, data_type, v[DATA_START_INDEX])
 						end
 					end
 				end
